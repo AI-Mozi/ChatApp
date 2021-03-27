82a8a262c27ef74c673442d6b8c0042e99f06dfa
AI-Mozi <bieniek.milosz0@gmail.com>
2021-03-27 15:59:44 +0100

    Production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'mozi-chat.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'mozi-chat.herokuapp.com',
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    authentcation: 'plain',
+    enable_starttls_auto: true
+  }
 end
