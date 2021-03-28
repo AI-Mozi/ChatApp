c062eb20e55fda2bb92e04862e0205372a348a55
AI-Mozi <bieniek.milosz0@gmail.com>
2021-03-28 18:57:34 +0200

    Added active users and some frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   helper_method :all_rooms
   before_action :all_rooms
+  before_action :set_online
 
-  private 
-  
+  private
+
   def all_rooms
     @rooms = Room.all
+  end
+
+  def set_online
+    if !!current_user
+      $redis_onlines.set( current_user.id, nil, ex: 60 )
+    end
   end
 end
