4a0752d244e8aa42b0e05d84a541ae94018fe923
AI-Mozi <bieniek.milosz0@gmail.com>
2021-03-29 10:33:01 +0200

    Room list styling

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy,
                         inverse_of: :room
+  validates :name, presence: true, length: {minimum: 1, maximum: 34}
 end
